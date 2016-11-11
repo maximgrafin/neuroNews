884525d5ab8f8597de5dd1e7180ae36c8cd8737b
Vlad E <vlad@pixzillavspixkong.com>
2016-11-11 22:02:43 +0100

    datareader improved

--- js/datareader.js
+++ js/datareader.js
@@ -9,18 +9,46 @@ module.exports =
         var bdt_ref = firebase.database().ref('/feuerfrei/aufschaltung/ht0/feed/wire/article/dpasrv_bdt/latest/entries');
 
         bdt_ref.limitToLast(100).on('value', show);
+
+        console.log('hello world');
     }
 };
 
-function show(snapshot) {
+function getDataAsArray(entry)
+{
+    var metadata;
     var metaDataExtractor = require('./metaDataExtractor');
+    metadata = metaDataExtractor.getMetaData(entry);
+    metadata.unshift(1);
+    metadata.push(entry.title.replace(/[\n\,]/g, ''));
+    return metadata;
+}
+
+function getPredictionServiceJson(entry)
+{
+    var metadata = getDataAsArray(entry);
+    var text = '{ "Inputs": { "input1":  [ { ';
+    for (i = 0; i < metadata.length; i++) { 
+        text += '\'Col' + (i+1) + '\':\"' + metadata[i]  + '",';
+    }
+    text = text.substring(0, text.length - 1);
+    text += '} ], },"GlobalParameters":  { } }';
+    console.log(text);
+
+    return text;
+}
+
+function show(snapshot) 
+{
     var fs = require('fs');
     var entries = snapshot.val();
     fs.writeFileSync('./output/out.txt', "");
-    for (var k in entries) {
+    for (var k in entries) 
+    {
         var entry = entries[k];
-        var metadata = metaDataExtractor.getMetaData(entry).join(',');
-        var row = '1,' + metadata + ',' + entry.title.replace(/[\n\,]/g, '') ;
+        var metadata = getDataAsArray(entry);
+        getPredictionServiceJson(entry);
+        var row = metadata.join(',');
         fs.appendFileSync('./output/out.txt', row + '\n');
         console.log(entry.id + ',' + row);
     }

0b0e4dce8c17a71e98f3035423e1124d99f50a78
Vlad E <vlad@pixzillavspixkong.com>
2016-11-11 22:12:41 +0100

    predictive service

--- js/datareader.js
+++ js/datareader.js
@@ -24,6 +24,60 @@ function getDataAsArray(entry)
     return metadata;
 }
 
+function getPrediction(entry)
+{
+    var url = 'https://europewest.services.azureml.net/workspaces/47d4f830adac4031953eadd43c9f816f/services/e906063e6aff4920a0d1e479798912e1/execute?api-version=2.0&format=swagger';
+    var api_key = 'hlXQ8mZXAjCX+BTH7V0fv4z03KfxGMDgoL0v/VNS33XdXfT2Z/DLOJhzMhHzpcKBxVM6rwQ/onUrflw+y5PHlA==';
+    var headers = { 'Content-Type':'application/json', Authorization: 'Bearer '+ api_key};
+    
+    performRequest(url, 'POST', headers, getPredictionServiceJson(entry), function(data) {
+    sessionId = data.result.id;
+    console.log('Logged in:', sessionId);
+  });
+}
+
+function performRequest(endpoint, method, headers, data, success) 
+{
+  var querystring = require('querystring');
+  var https = require('https');
+
+  var dataString = JSON.stringify(data);
+  
+  if (method == 'GET') 
+  {
+    endpoint += '?' + querystring.stringify(data);
+  }
+
+  var options = 
+  {
+    host: host,
+    path: endpoint,
+    url: end //fix here
+        method: method,
+    headers: headers
+  };
+
+  var req = https.request(options, function(res) 
+  {
+    res.setEncoding('utf-8');
+
+    var responseString = '';
+
+    res.on('data', function(data) {
+      responseString += data;
+    });
+
+    res.on('end', function() {
+      console.log(responseString);
+      var responseObject = JSON.parse(responseString);
+      success(responseObject);
+    });
+  });
+
+  req.write(dataString);
+  req.end();
+}
+
 function getPredictionServiceJson(entry)
 {
     var metadata = getDataAsArray(entry);
@@ -47,7 +101,7 @@ function show(snapshot)
     {
         var entry = entries[k];
         var metadata = getDataAsArray(entry);
-        getPredictionServiceJson(entry);
+        getPrediction(entry);
         var row = metadata.join(',');
         fs.appendFileSync('./output/out.txt', row + '\n');
         console.log(entry.id + ',' + row);

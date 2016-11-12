7fc255b5784d8446ef9daebf39dfbf9c21d399a7
Vlad E <vlad@pixzillavspixkong.com>
2016-11-12 02:45:23 +0100

    Added data layer

--- app/src/AppController.js
+++ app/src/AppController.js
@@ -1,3 +1,34 @@
+var predictiveservices = require('../../js/predictiveservices');
+var common = require('../../js/common');
+var metaDataExtractor = require('../../js/metaDataExtractor');
+
+function ShowData()
+{
+    var firebase = require('firebase');
+    var Promise = require('promise');
+    var escape = require('escape-html');
+
+    firebase.initializeApp({databaseURL: "https://dpahthon1611.firebaseio.com"});
+    var bdt_ref = firebase.database().ref('/feuerfrei/aufschaltung/ht0/feed/wire/article/dpasrv_bdt/latest/entries');
+
+    bdt_ref.limitToLast(2).on('value', show);
+}
+
+function show(snapshot) 
+{
+    var entries = snapshot.val();
+    for (var k in entries) 
+    {
+        var pieceOfNews = entries[k];
+        var metadata = metaDataExtractor.getMetaData(pieceOfNews);
+        var row = metadata.map(x => common.getJsonValue(x)).join(',');
+        //change color here or something
+        predictiveservices.predictEntryScore(entry, function(data) {console.log('Predicted=' + data);});
+    }
+};
+
+
+
 function AppController(NewsDataService) {
     var self = this;
 
